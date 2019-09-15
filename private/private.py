3f41c9c7b7882ea46aa4e20ecb113d04552b8e19
NicolasGagne <ngagne966@gmail.com>
2019-09-14 21:15:12 -0400

    Add the Original private.py as a moke up

---
+++ private/private.py
@@ -0,0 +1,20 @@
+"""
+All private information for the login and other
+
+
+Replace None with your private information
+"""
+
+
+"""
+DDNS PROVIDER
+I use https://www.dynu.com 
+You have to modified the code fi you use another proveder
+"""
+
+DNS_PROVIDER_LOGIN = None
+
+DNS_LOGIN = None
+DNS_PASSWORD = None
+
+
